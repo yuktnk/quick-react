af95d347ec52a8a8c3f38f5e6d0d219be3568d83
yuktnk <yuki0wl@gmail.com>
2020-09-11 23:31:38 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+//1.React関連のライブラリをインポート
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//2.アプリ固有のファイルをインポート
 import './index.css';
 import App from './App';
+
+//3.キャッシュのためのサービスをインポート
 import * as serviceWorker from './serviceWorker';
 
+//4.Appコンポーネントを実行
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +20,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+//5.サービスを有効化
 serviceWorker.unregister();
