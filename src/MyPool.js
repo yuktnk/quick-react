94260fb001a63aa2e29696b03da317489d2d480d
yuktnk <yuki0wl@gmail.com>
2020-09-14 11:51:14 +0900

    Update MyPool.js
    event.persistを用いて元のSyntheticEventを維持

--- src/MyPool.js
+++ src/MyPool.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 export default class MyPool extends Component {
   // ボタンクリック時にログ出力
   show(e) {
-    console.log(e.type); //  結果：click・・・①
+    e.persist();
+    console.log(e.type); //  結果：click
     setTimeout(()=>{
-      console.log(e.type); // 結果：null・・・②
+      console.log(e.type); // 結果：null
     }, 1000);
   }
   render() {
