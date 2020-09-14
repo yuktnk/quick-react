cbe116fe1b8bee1fdac71091a6c16e28361cb54c
yuktnk <yuki0wl@gmail.com>
2020-09-14 09:24:58 +0900

    [update]MyEvent.js
    bindメソッドの第二引数を利用し、イベントハンドラーに引数を渡す。

--- src/MyEvent.js
+++ src/MyEvent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 export default class MyEvent extends Component {
   // 入力ボックス変更時にメッセージを出力
-  show(e) {
+  show(suffix,e) {
     // 3. greetプロパティ、入力値に基づいて、メッセージを生成するメソッド
-    console.log(`${this.props.greet}, ${e.target.value}!!`);
+    console.log(`${this.props.greet}, ${e.target.value}${suffix}!!`);
   }
   constructor(props) {
     super(props)
@@ -14,9 +14,9 @@ export default class MyEvent extends Component {
       <form>
         <label htmlFor="txtName">名前：</label>
         {/* 本当はidは一意じゃなきゃダメ。 */}
-        
+
         {/* 1. 2. 入力内容変更時にshowメソッドを呼び出す */}
-        <input id="txtName" type="text" onChange={this.show.bind(this)} />
+        <input id="txtName" type="text" onChange={this.show.bind(this,'さん')} />
 
         {/* コンストラクターでbind */}
         < input id="txtName" type="text" onChange={this.show} />
