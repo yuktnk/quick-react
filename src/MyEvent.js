7ca0815a4735713be4ceda3c5e4be5c48fbcb553
yuktnk <yuki0wl@gmail.com>
2020-09-14 09:13:34 +0900

    [update]MyEvent.js
    アロー関数で宣言

--- src/MyEvent.js
+++ src/MyEvent.js
@@ -13,9 +13,16 @@ export default class MyEvent extends Component {
     return (
       <form>
         <label htmlFor="txtName">名前：</label>
+        {/* 本当はidは一意じゃなきゃダメ。 */}
+        
         {/* 1. 2. 入力内容変更時にshowメソッドを呼び出す */}
-        {/* <input id="txtName" type="text" onChange={this.show.bind(this)} /> */}
+        <input id="txtName" type="text" onChange={this.show.bind(this)} />
+
+        {/* コンストラクターでbind */}
         < input id="txtName" type="text" onChange={this.show} />
+
+        {/* アロー関数 */}
+        < input id="txtName" type="text" onChange={(e)=>this.show(e)} />
       </form>
     );
   }
