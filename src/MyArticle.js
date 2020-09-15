f61cd3a8d5fc3631b078b99f0a135b9539b1f35f
yuktnk <yuki0wl@gmail.com>
2020-09-15 19:14:41 +0900

    [update]MyArticle.js
    コンポーネントを入れ子に配置

--- src/MyArticle.js
+++ src/MyArticle.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
+import MyPage from './MyPage';
 
 export default class MyArticle extends Component {
   render() {
     const id = this.props.match.params.id;
+    const path = this.props.match.path;
+    const url = this.props.match.url;
     return (
-      <div>No. {id}の記事情報です！</div>
+      <div>
+        <h3>No. {id}の記事情報です！</h3>
+        {/* 子コンポーネントへのリンク・・・② */}
+        [<Link to={`${url}/pages/1`}>1</Link>]
+        [<Link to={`${url}/pages/2`}>2</Link>]
+        <hr />
+        {/* 「/article/:id/pages/:page」へのルートを宣言 */}
+        <Route path={`${path}/pages/:page`} component={MyPage} />
+      </div>
     );
   }
 }
